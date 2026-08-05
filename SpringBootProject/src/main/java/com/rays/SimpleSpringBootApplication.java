e449402803fd192645c3d9cfd7e9410d284bbc71
vishwakarmadeepak646 <vishwakarmadeepak646@gmail.com>
2026-08-05 23:47:12 +0530

    Done

--- SpringBootProject/src/main/java/com/rays/SimpleSpringBootApplication.java
+++ SpringBootProject/src/main/java/com/rays/SimpleSpringBootApplication.java
@@ -1,14 +1,34 @@
 package com.rays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.rays.common.FrontCtl;
 
 @SpringBootApplication
 public class SimpleSpringBootApplication {
-	
+
+	@Autowired
+	FrontCtl frontCtl;
+
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(SimpleSpringBootApplication.class, args);
+	}
+
+	public WebMvcConfigurer webConfig() {
+
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+
+				registry.addInterceptor(frontCtl).addPathPatterns("/**").excludePathPatterns("/Auth/*");
+			}
+		};
 	}
 
 }
---
+++ SpringBootProject/src/main/java/com/rays/exception/DuplicateRecordException.java
@@ -0,0 +1,9 @@
+package com.rays.exception;
+
+public class DuplicateRecordException extends RuntimeException {
+	
+
+	public DuplicateRecordException(String msg) {
+		super(msg);
+	}
+}
